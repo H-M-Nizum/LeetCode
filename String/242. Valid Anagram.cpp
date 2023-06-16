73d49afe55d7f4eaf07d65bf860c78dfbf0cb38d
H-M-Nizum <hmnizum1714032@gmail.com>
2023-06-16 23:43:09 +0600

    242

---
+++ String/242. Valid Anagram.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, t;
+    cin>>s>>t;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if(s == t) cout<<"true";
+    else cout<<"false";
+    
+	return 0;
+}
