73b8ad20ecaf687a864bd49a8a03558a6d051f64
Nizum <hmnizum1714032@gmail.com>
2023-08-02 10:46:35 +0600

    Backtracking

---
+++ Backtracking/47. Permutations II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void display(vector<int> nums, int n, vector<int> cur, vector<vector<int>> &tem){
+        for (int i = 0; i < n; i++) {
+            cur.push_back(nums[i]);
+        }
+        tem.push_back(cur);
+        cur.clear();
+    }
+    
+    void findPermutations(vector<int> nums, int n, vector<int>cur, vector<vector<int>> &tem){
+        
+        sort(nums.begin(), nums.end());
+        
+        do {
+            display(nums, n, cur,tem);
+        } while (next_permutation(nums.begin(), nums.end()));
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> tem;
+        vector<int> cur = {};
+        findPermutations(nums, n, cur, tem);
+        return tem;
+    }
+};
